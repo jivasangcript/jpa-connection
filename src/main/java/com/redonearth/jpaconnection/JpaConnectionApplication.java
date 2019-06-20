b0c0fadb86878174a3480076b135664ef3e7d925
jivasangcript <wona23@gmail.com>
2019-06-20 22:44:21 +0900

    project init

---
+++ src/main/java/com/redonearth/jpaconnection/JpaConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.redonearth.jpaconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaConnectionApplication.class, args);
+    }
+
+}
